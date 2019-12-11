3038c4ce0583b9d9f6cc44ca9caa92105ecf6d05
Doirena <barkauskaite.dovile@gmail.com>
2019-12-12 01:34:07 +0200

    correct arrow movement

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 const arrowStyle = document.querySelector('.arrow');
+const initialPositionBtn = 53;
 function arrowMove(e){
-    // let step = 0; 
-    console.log(this.dataset.name);
+    console.log(e);
+    // console.log(this.dataset.name);
+    
+    
+    // Pirmasis mygtukas greicio i prieki
     if (this.dataset.name === 'start'){
-        for (let index = 0; index < 250; index++) {
-         arrowStyle.style.transform = `rotate(${index}deg)`;    
+        for (let index = 0; index <= 232; index++) {
+         arrowStyle.style.transform = `rotate(${index}deg)`; 
+         arrowStyle.style.transition = `transform ${500}ms`; 
+         arrowStyle.style.background = `red`;   
+        } 
+    } 
+    //kai atleidziamas pelesmygtukas
+    firstBtn.forEach(button => button.addEventListener ('mouseup', () => {
+    if (this.dataset.name === 'start'){
+        arrowStyle.style.transform = `rotate(${-initialPositionBtn}deg)`;
+        arrowStyle.style.transition = `transform ${5}s`;
+        
         }
-    }
+    }));
+
     if(this.dataset.name === 'stop'){
-        arrowStyle.style.transform = `rotate(${-53}deg)`
+        arrowStyle.style.transform = `rotate(${-initialPositionBtn}deg)`
+        arrowStyle.style.transition = `transform ${500}ms`;  
+        arrowStyle.style.background = `green`; 
     }  
 }
 
 const firstBtn = document.querySelectorAll('.button');
 firstBtn.forEach(button => button.addEventListener ('mousedown',arrowMove));
+// firstBtn.forEach(button => button.addEventListener ('mouseleave', () => mousedown =false));
+
+
+
