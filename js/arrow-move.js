11f626aac29be88e9089e01db86bb742be1b309f
Doirena <barkauskaite.dovile@gmail.com>
2019-12-16 15:08:46 +0200

    arrow movement doen

--- js/arrow-move.js
+++ js/arrow-move.js
@@ -5,23 +5,29 @@ const secondButton = document.querySelector("#button2");
 const arrowStyle = document.querySelector('.arrow');
 
     let timerID;
+    let timerID1;
     let counter = -53;
+    let counter1 = -53;
 
  
     const pressHoldEvent = new CustomEvent("pressHold");
     //Galimas ispaudimo laikas kai kad rodykle apsisuktu, t.y. 
     const pressHoldDuration = 232;
+    const pressHoldDuration1 = 232;
  
     // Listening for the mouse and touch events    
     firstButton.addEventListener("mousedown", pressingDown);
     firstButton.addEventListener("mouseup", notPressingDown);
     firstButton.addEventListener("mouseleave", notPressingDown);
    
-    secondButton.addEventListener('mousedown', pressDownStop);
-    secondButton.addEventListener("mouseup", notPressingDown);
+    // secondButton.addEventListener('mousedown', pressDownStop);
+    // secondButton.addEventListener("mouseup", notPressingDown);
+    secondButton.addEventListener("mouseup", pressingDown1);
+    secondButton.addEventListener("mousedown", notPressingDown1);
+    secondButton.addEventListener("mouseleave", notPressingDown1);
 
     function pressDownStop(e){
-      cancelAnimationFrame(timerID);
+      // cancelAnimationFrame(timerID);
       arrowStyle.style.transform = `rotate(${-53}deg)`;
       arrowStyle.style.transition = `transform ${5}ms`;          
       console.log("Pressing! Stop");
@@ -56,6 +62,41 @@ const arrowStyle = document.querySelector('.arrow');
       } else {
         console.log("Press threshold reached!");
         firstButton.dispatchEvent(pressHoldEvent);
+      }
+      
+    }
+
+
+
+
+    //antrajam btn
+    function pressingDown1(e) {
+      // Start the timer
+      requestAnimationFrame(timerback);      
+      e.preventDefault();
+      console.log("Pressing!");
+    }
+    function notPressingDown1(e) {
+      // Stop the timer
+      cancelAnimationFrame(timerID1);
+      counter1 = -53;
+      arrowStyle.style.transform = `rotate(${counter1}deg)`;
+      arrowStyle.style.transition = `transform ${400}ms`;     
+      console.log("Not pressing!");
+    }
+
+
+
+    function timerback() {
+      console.log("Timer tick!");
+ 
+      if (counter1 < pressHoldDuration1) {
+        timerID1 = requestAnimationFrame(timerback);
+        counter1++;
+        arrowStyle.style.transform = `rotate(${-52}deg)`;    
+      } else {
+        console.log("Press threshold reached!");
+        secondButton.dispatchEvent(pressHoldEvent);
       }
       
     }
